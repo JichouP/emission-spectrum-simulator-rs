983ec3320a660d7e491c7656b76527cd49e5c371
JichouP <akkunn100@gmail.com>
2022-10-30 18:00:06 +0900

    :sparkles: add calculate cmd

---
+++ src-tauri/src/cmd/calculate.rs
@@ -0,0 +1,84 @@
+use emission_spectrum_lib_rs::prelude::*;
+
+#[command]
+pub fn calculate(
+    t_r: f64,
+    t_v: f64,
+    wave_length_start: f64,
+    wave_length_range: f64,
+    resolution: f64,
+) -> Vec<(f64, f64)> {
+    let increment = wave_length_range / resolution;
+    let wave_lengths: Vec<f64> = (0..resolution as usize)
+        .map(|v| wave_length_start + increment * v as f64)
+        .collect();
+
+    println!("{:?}", wave_lengths.len());
+
+    let config = CalcConfig {
+        dunham_expression_params_u: vec![
+            vec![0.0, 1153.3, -19.48, -0.4],
+            vec![1.320, -0.022],
+            vec![-4.0e-6],
+        ],
+        dunham_expression_params_l: vec![
+            vec![0.0, 1308.1, -11.10, 0.093],
+            vec![1.4172, -0.0184, 0.00011],
+            vec![-6.5e-6],
+        ],
+        fwhm: 5.0e-10,
+        lu: 2.0,
+        ll: 1.0,
+        q: vec![
+            (0, 0, 0.6765),
+            (0, 1, 0.2821),
+            (1, 1, 0.2316),
+            (1, 2, 0.4801),
+            (2, 2, 0.0297),
+            (2, 3, 0.3935),
+        ],
+        r: 0.0,
+        spin_configs: vec![
+            (
+                CouplingKind::B,
+                DoubletBranchKind::P1,
+                (6..21).map(|v| v as f64 + 0.5).collect(),
+            ),
+            (
+                CouplingKind::B,
+                DoubletBranchKind::P2,
+                (6..21).map(|v| v as f64 + 0.5).collect(),
+            ),
+            (
+                CouplingKind::B,
+                DoubletBranchKind::Q1,
+                (2..26).map(|v| v as f64 + 0.5).collect(),
+            ),
+            (
+                CouplingKind::B,
+                DoubletBranchKind::Q2,
+                (4..25).map(|v| v as f64 + 0.5).collect(),
+            ),
+            (
+                CouplingKind::B,
+                DoubletBranchKind::R1,
+                (1..26).map(|v| v as f64 + 0.5).collect(),
+            ),
+            (
+                CouplingKind::B,
+                DoubletBranchKind::R2,
+                (3..26).map(|v| v as f64 + 0.5).collect(),
+            ),
+        ],
+        spin_quantum_number_kind: SpinQuantumNumberKind::Doublet,
+        t_e_u: Term::new(49399.6),
+        t_e_l: Term::new(0.0),
+        t_r: Term::new(t_r),
+        t_v: Term::new(t_v),
+        wave_lengths: wave_lengths.to_vec(),
+    };
+
+    let result = calc(config);
+
+    wave_lengths.into_iter().zip(result).collect()
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,14 @@
     windows_subsystem = "windows"
 )]
 
+#[macro_use]
+extern crate tauri;
+
+mod cmd;
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![cmd::calculate::calculate])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
