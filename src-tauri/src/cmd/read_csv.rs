736d5ab704bb8c69952e954be0a9aecacc7d1c8b
JichouP <akkunn100@gmail.com>
2022-10-31 22:09:23 +0900

    :sparkles: normalization

--- src-tauri/src/cmd/read_csv.rs
+++ src-tauri/src/cmd/read_csv.rs
@@ -1,13 +1,22 @@
 #[command]
-pub fn read_csv(path: &str) -> Result<Vec<f64>, String> {
+pub fn read_csv(path: &str) -> Result<Vec<(f64, f64)>, String> {
     let mut reader = csv::ReaderBuilder::new();
     reader.has_headers(false);
     let result = reader
         .from_path(path)
         .map_err(|e| format!("error: {}", e))?
         .records()
-        .filter_map(|v| v.ok()?.get(0)?.parse::<f64>().ok())
+        .filter_map(|v| {
+            let v = v.ok()?;
+            let v0 = v.get(0)?.parse::<f64>().ok()?;
+            let v1 = v.get(1)?.parse::<f64>().ok()?;
+            Some((v0, v1))
+        })
         .collect::<Vec<_>>();
+
+    // normalization
+    let max = result.iter().copied().map(|v| v.1).fold(f64::NAN, f64::max);
+    let result = result.into_iter().map(|(a, b)| (a, b / max)).collect();
 
     Ok(result)
 }
@@ -18,6 +27,16 @@ mod tests {
     #[test]
     fn test() {
         let result = read_csv("example/example.csv");
-        assert_eq!(result.unwrap(), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+        assert_eq!(
+            result.unwrap(),
+            vec![
+                (1.0, 1.0),
+                (2.0, 2.0),
+                (3.0, 3.0),
+                (4.0, 4.0),
+                (5.0, 5.0),
+                (6.0, 6.0)
+            ]
+        );
     }
 }
